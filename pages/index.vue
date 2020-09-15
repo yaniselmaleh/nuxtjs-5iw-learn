8b62c4507df05032ba831fcdc38a05f49ba09a84
yaniselmaleh <contact@yaniselmaleh.fr>
2020-09-15 12:17:55 +0200

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle">
-        My astonishing Nuxt.js project
+        My astonishing Nuxt.js project 123
       </h2>
       <div class="links">
         <a
